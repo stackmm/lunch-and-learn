215caa6ed8d7a2250f35fd8df5c4f499f6497251
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 15:19:42 -0600

    add tests

--- spec/requests/api/v1/learning_resources_request_spec.rb
+++ spec/requests/api/v1/learning_resources_request_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "Find Learning Resources by Country API", type: :request do
   describe "GET /api/v1/learning_resources" do
+    # data attribute shouod have keys of id (null), type (learning_resource), and attributes
+    # attributes should have keys of country (country name)
+    # video (title: title of video, youtube_video_id: video id from youtube),
+    # images:, a collection holding elements that have info about the image (alt_tag, url for image)
     describe "happy path" do
       it "can find learning resources by country" do
         get "/api/v1/learning_resources?country=laos"
