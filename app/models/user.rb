1003eda1d9701ec07efa2bd612f33b90961c1c9e
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 17:57:38 -0600

    add model and test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true, presence: true
+
+  before_create :generate_api_key
+
+  private
+
+  def generate_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_confirmation_of :password }
+    it { should validate_presence_of :password }
+  end
+
+  describe "instance methods" do
+    describe "#generate_api_key" do
+      it "should generate an api key" do
+        user = User.create!(name: "test", email: "stackmm@gmail.com", password: "password", password_confirmation: "password")
+      end
+    end
+  end
 end
