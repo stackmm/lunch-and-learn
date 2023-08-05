6fffc1665836c9d9c34cc4934cc04d8a0e5b8bef
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 17:37:21 -0600

    remove comments

--- app/facades/learning_resource_facade.rb
+++ app/facades/learning_resource_facade.rb
@@ -1,7 +1,5 @@
 class LearningResourceFacade
   def self.get_resources(country)
-    # return [] if country.nil?
-
     video = YoutubeService.get_video_by_country(country)
     images = UnsplashService.get_images_by_country(country)
 
