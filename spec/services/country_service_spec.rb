1960d04c88de8afcd095212f0ca061c4aa572efe
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 11:03:17 -0600

    update casettes

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -3,6 +3,5 @@ require "rails_helper"
 RSpec.describe CountryService do
   it "establishes a connection and obtains a random country name", :vcr do
     country = CountryService.new.get_random_country
-    require 'pry'; binding.pry
   end
 end
