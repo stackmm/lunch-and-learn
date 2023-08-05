e16b678e6cf597801199ab08a2ea8a0c2d03f5a2
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 11:24:21 -0600

    Add country search

--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -1,15 +1,16 @@
 class CountryService
-  def conn
-    Faraday.new(url: "https://restcountries.com/v3.1")
+  def self.conn
+    Faraday.new(url: "https://restcountries.com/v3.1/")
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def get_random_country
-    countries = get_url("/all")
+  def self.get_country(country)
+    get_url("/name/#{country}")
+    # countries = get_url("/all")
     # countries.sample[:name][:common]
   end
 end
