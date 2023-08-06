8f6f64d477e3dcc0f787e4775c5d967205743a3e
Steven Stackhouse <stackmm@gmail.com>
2023-08-06 16:54:26 -0600

    add sath path tests

--- spec/requests/api/v1/add_favorites_request.rb
+++ spec/requests/api/v1/add_favorites_request.rb
@@ -26,7 +26,25 @@ RSpec.describe "Add Favorites API", type: :request do
     end
 
     describe "sad path" do
+      it "returns an error if the user's API key is invalid" do
+        user = User.create!(name: "Mikejones", email: "mjones@gmail.com", password: "password", password_confirmation: "password")
 
+        favorite_params = {
+          api_key: "1111",
+          country: "thailand",
+          recipe_link: "https://www.seriouseats.com/recipes/2013/11/andy-rickers-naam-cheuam-naam-taan-piip-palm-sugar-simple-syrup.html",
+          recipe_title: "Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)"
+        }
+
+        post "/api/v1/favorites", params: favorite_params
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(400)
+
+        favorite = JSON.parse(response.body, symbolize_names: true)
+
+        expect(favorite[:error]).to eq("Invalid API Key")
+      end
     end
   end
 end
