a67fe5e5aa6f518072d34fa8f82371feeb88dffc
Steven Stackhouse <stackmm@gmail.com>
2023-08-06 16:42:51 -0600

    Add validations

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Favorite, type: :model do
   describe "relationships" do
     it { should belong_to :user }
   end
+
+  describe "validations" do
+    it { should validate_presence_of :country }
+    it { should validate_presence_of :recipe_link }
+    it { should validate_presence_of :recipe_title }
+  end
 end
