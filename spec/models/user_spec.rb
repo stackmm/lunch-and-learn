0fd44bc8bb18f1d6dd00316f4b3050ae83a962a3
Steven Stackhouse <stackmm@gmail.com>
2023-08-05 20:45:15 -0600

    add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe User, type: :model do
     describe "#generate_api_key" do
       it "should generate an api key" do
         user = User.create!(name: "test", email: "stackmm@gmail.com", password: "password", password_confirmation: "password")
+        expect(user.api_key).to_not eq(nil)
       end
     end
   end
