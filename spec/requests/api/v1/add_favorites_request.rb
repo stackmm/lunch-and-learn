daa1ac9503b7d807a8b07a746ab8914f63c13204
Steven Stackhouse <stackmm@gmail.com>
2023-08-06 16:01:18 -0600

    create test for adding favorites service

---
+++ spec/requests/api/v1/add_favorites_request.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Add Favorites API", type: :request do
+  describe "POST /api/v1/favorites" do
+    describe "happy path" do
+      it "can add a favorite for a user when given valid parameters" do
+        user = User.create!(name: "Mikejones", email: "mjones@gmail.com", password: "password", password_confirmation: "password")
+
+        favorite_params = {
+          api_key: user.api_key,
+          country: "thailand",
+          recipe_link: "https://www.seriouseats.com/recipes/2013/11/andy-rickers-naam-cheuam-naam-taan-piip-palm-sugar-simple-syrup.html",
+          recipe_title: "Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)"
+        }
+
+        post "/api/v1/favorites", params: favorite_params
+
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+
+        favorite = JSON.parse(response.body, symbolize_names: true)
+
+        expect(favorite).to be_a(Hash)
+        expect(favorite[:success]).to eq("Favorite added successfully")
+      end
+    end
+
+    describe "sad path" do
+
+    end
+  end
+end
