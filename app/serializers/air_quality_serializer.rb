56dd9eddb8df4be22783af3909ce0ac583f23ce9
Steven Stackhouse <stackmm@gmail.com>
2023-08-07 10:27:11 -0600

    Refactor serializer

--- app/serializers/air_quality_serializer.rb
+++ app/serializers/air_quality_serializer.rb
@@ -1,10 +1,5 @@
 class AirQualitySerializer
   include JSONAPI::Serializer
-  set_id :null_id
   set_type :air_quality
   attributes :city, :aqi, :pm25_concentration, :co_concentration
-
-  def null_id
-    nil
-  end
 end
