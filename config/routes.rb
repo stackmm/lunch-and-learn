e398a87b80d09aa88a9ac3c55b68e637e3b8ce27
Steven Stackhouse <stackmm@gmail.com>
2023-08-07 10:06:46 -0600

    add route and refactor test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      get "/air_quality", to: "air_quality#show"
       get "/recipes", to: "recipes#index"
       get "/learning_resources", to: "learning_resources#index"
       get "/favorites", to: "favorites#index"
