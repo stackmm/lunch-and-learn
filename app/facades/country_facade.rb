fabf908af8810c3f8e0906893c7a728d010e47b8
Steven Stackhouse <stackmm@gmail.com>
2023-08-07 10:36:51 -0600

    update country facade

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,6 +1,11 @@
 class CountryFacade
   def self.get_random_country
     response = CountryService.get_random_country
-    Country.new(response)
+    Country.new(response[:name][:common], response[:capital].first)
+  end
+
+  def self.get_country_by_name(country)
+    response = CountryService.get_country_by_name(country)
+    Country.new(response[:name][:common], response[:capital].first)
   end
 end
