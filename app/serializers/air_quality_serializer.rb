599864fa37d76a7764c17347c45235d97dca5c64
Steven Stackhouse <stackmm@gmail.com>
2023-08-07 10:17:40 -0600

    add method for setting id to nil

--- app/serializers/air_quality_serializer.rb
+++ app/serializers/air_quality_serializer.rb
@@ -1,6 +1,10 @@
 class AirQualitySerializer
   include JSONAPI::Serializer
-  set_id :id
+  set_id :null_id
   set_type :air_quality
   attributes :city, :aqi, :pm25_concentration, :co_concentration
+
+  def null_id
+    nil
+  end
 end
