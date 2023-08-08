1c4433099363d6a58e3a568893e680b70a3ea1b3
Steven Stackhouse <stackmm@gmail.com>
2023-08-08 11:17:38 -0600

    Refactor and add destroy to favorites

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      get "/recipes", to: "recipes#index"
-      get "/learning_resources", to: "learning_resources#index"
-      get "/favorites", to: "favorites#index"
-      post "/favorites", to: "favorites#create"
-      post "/sessions", to: "sessions#create"
+      resources :favorites, only: [:index, :create, :destroy]
+      resources :recipes, only: [:index]
+      resources :learning_resources, only: [:index]
+      resources :sessions, only: [:create]
     end
   end
 end
